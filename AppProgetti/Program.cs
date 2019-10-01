06e4e1a0621c2f31367f9d74156c093034e38c2b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-01 09:56:48 +0200

    v

--- AppProgetti/Program.cs
+++ AppProgetti/Program.cs
@@ -12,9 +12,28 @@ namespace AppProgetti
         static void Main(string[] args)
         {
             Console.WriteLine("Esercitazione sulla gestione dei processi");
+
             Process.Start(@"\\DC01SRV\_Condivisa\INF\4 A\Palugini Simone\AppProgetti\AppProgetti\HelloWorld.txt");
             Process.Start("Chrome.exe", @"https://www.inter.it/it/squadra/G0943/tommaso-berni");
 
+            var app = new Process();
+            app.StartInfo.FileName = @"Notepad.exe";
+            app.StartInfo.Arguments = @"\\DC01SRV\_Condivisa\INF\4 A\Palugini Simone\AppProgetti\AppProgetti\HelloWorld.txt";
+            app.Start();
+            app.PriorityClass = ProcessPriorityClass.RealTime;
+            //app.WaitForExit();
+            
+
+            var processes = Process.GetProcesses();
+            foreach(var p in processes)
+            {
+                if (p.ProcessName = "Notepad")
+                {
+                    p.Kill();
+                }
+            }
+
+            Console.WriteLine("Programma terminato");
             Console.ReadLine();
         }
     }
