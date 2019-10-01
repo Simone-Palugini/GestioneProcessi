8f9fd41fb28b55347d55df14a1921d9c6cc70e7e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-01 09:26:07 +0200

    aggiunti 2 "Process.Start" per aprire un file di testo e una pagina su google chrome

--- AppProgetti/Program.cs
+++ AppProgetti/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,11 @@ namespace AppProgetti
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Esercitazione sulla gestione dei processi");
+            Process.Start(@"\\DC01SRV\_Condivisa\INF\4 A\Palugini Simone\AppProgetti\AppProgetti\HelloWorld.txt");
+            Process.Start("Chrome.exe", @"https://www.inter.it/it/squadra/G0943/tommaso-berni");
 
+            Console.ReadLine();
         }
     }
 }
